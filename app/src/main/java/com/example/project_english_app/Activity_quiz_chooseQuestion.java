2a97abfaf603f9a7f3b055063b9ea3733354a182
khangfutsal <khangfutsal2@gmail.com>
2022-12-10 11:01:35 +0700

    [update][10-12-2022][Khang]

--- app/src/main/java/com/example/project_english_app/Activity_quiz_chooseQuestion.java
+++ app/src/main/java/com/example/project_english_app/Activity_quiz_chooseQuestion.java
@@ -14,24 +14,32 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity_quiz_chooseQuestion extends Activity {
-    private Button btnQzStart;
+    private Button btnQzStart5,btnQzStart6,btnQzStart7,btnQzStart8,btnQzStart9,btnQzStart10;
+
     public int cauhoi;
     EditText N1;
+    Button BtnBack;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_choosequestion);
 //        String Cauhoi = String.valueOf((EditText) findViewById(R.id.choosequestion));
 
-        N1 = (EditText) findViewById(R.id.choosequestion);
-
-        btnQzStart = (Button) findViewById(R.id.btn_StartChoose);
-        btnQzStart.setOnClickListener(new View.OnClickListener() {
+        BtnBack = (Button)findViewById(R.id.BtnBack);
+        BtnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        btnQzStart5 = (Button) findViewById(R.id.btn_SL5);
+        btnQzStart5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Activity_quiz_chooseQuestion.this, Activity_quiz_play.class);
-                int A = Integer.parseInt(N1.getText().toString());
+                int A = 5 ;
                 Bundle bundle = new Bundle();
                 bundle.putInt("SL1", A);
                 bundle.putInt("K", 0);
@@ -41,28 +49,30 @@ public class Activity_quiz_chooseQuestion extends Activity {
                 bundle.putInt("D", 0);
                 bundle.putInt("Z", 0);
                 intent.putExtra("MyPackage1", bundle);
-                if (A == 5) {
-
-                    startActivity(intent);
-                    finish();
-                } else if (A == 6) {
-                    startActivity(intent);
-                    finish();
-                } else if (A == 7) {
-                    startActivity(intent);
-                    finish();
-                } else if (A == 8) {
-                    startActivity(intent);
-                    finish();
-                } else if (A == 9) {
-                    startActivity(intent);
-                    finish();
+                startActivity(intent);
+                finish();
+//                if (A == 5) {
 //
-                } else if (A == 10) {
-                    startActivity(intent);
-                    finish();
-//
-                }
+//                    startActivity(intent);
+//                    finish();
+//                } else if (A == 6) {
+//                    startActivity(intent);
+//                    finish();
+//                } else if (A == 7) {
+//                    startActivity(intent);
+//                    finish();
+//                } else if (A == 8) {
+//                    startActivity(intent);
+//                    finish();
+//                } else if (A == 9) {
+//                    startActivity(intent);
+//                    finish();
+////
+//                } else if (A == 10) {
+//                    startActivity(intent);
+//                    finish();
+////
+//                }
 //                switch (A) {
 //                    case 5:
 //                        openActivity_quiz_play();
@@ -87,10 +97,123 @@ public class Activity_quiz_chooseQuestion extends Activity {
             }
 
         });
+        btnQzStart6 = (Button) findViewById(R.id.btn_SL6);
+        btnQzStart6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity_quiz_chooseQuestion.this, Activity_quiz_play.class);
+                int A = 6;
+                Bundle bundle = new Bundle();
+                bundle.putInt("SL1", A);
+                bundle.putInt("K", 0);
+                bundle.putInt("H", 0);
+                bundle.putInt("C", 0);
+                bundle.putInt("B", 0);
+                bundle.putInt("D", 0);
+                bundle.putInt("Z", 0);
+                intent.putExtra("MyPackage1", bundle);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnQzStart6 = (Button) findViewById(R.id.btn_SL6);
+        btnQzStart6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity_quiz_chooseQuestion.this, Activity_quiz_play.class);
+                int A = 6;
+                Bundle bundle = new Bundle();
+                bundle.putInt("SL1", A);
+                bundle.putInt("K", 0);
+                bundle.putInt("H", 0);
+                bundle.putInt("C", 0);
+                bundle.putInt("B", 0);
+                bundle.putInt("D", 0);
+                bundle.putInt("Z", 0);
+                intent.putExtra("MyPackage1", bundle);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnQzStart7 = (Button) findViewById(R.id.btn_SL7);
+        btnQzStart7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity_quiz_chooseQuestion.this, Activity_quiz_play.class);
+                int A = 7;
+                Bundle bundle = new Bundle();
+                bundle.putInt("SL1", A);
+                bundle.putInt("K", 0);
+                bundle.putInt("H", 0);
+                bundle.putInt("C", 0);
+                bundle.putInt("B", 0);
+                bundle.putInt("D", 0);
+                bundle.putInt("Z", 0);
+                intent.putExtra("MyPackage1", bundle);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnQzStart8 = (Button) findViewById(R.id.btn_SL8);
+        btnQzStart8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity_quiz_chooseQuestion.this, Activity_quiz_play.class);
+                int A = 8;
+                Bundle bundle = new Bundle();
+                bundle.putInt("SL1", A);
+                bundle.putInt("K", 0);
+                bundle.putInt("H", 0);
+                bundle.putInt("C", 0);
+                bundle.putInt("B", 0);
+                bundle.putInt("D", 0);
+                bundle.putInt("Z", 0);
+                intent.putExtra("MyPackage1", bundle);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnQzStart9 = (Button) findViewById(R.id.btn_SL9);
+        btnQzStart9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity_quiz_chooseQuestion.this, Activity_quiz_play.class);
+                int A = 9;
+                Bundle bundle = new Bundle();
+                bundle.putInt("SL1", A);
+                bundle.putInt("K", 0);
+                bundle.putInt("H", 0);
+                bundle.putInt("C", 0);
+                bundle.putInt("B", 0);
+                bundle.putInt("D", 0);
+                bundle.putInt("Z", 0);
+                intent.putExtra("MyPackage1", bundle);
+                startActivity(intent);
+                finish();
+            }
+        });
 
-    }
+        btnQzStart10 = (Button) findViewById(R.id.btn_SL10);
+        btnQzStart10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity_quiz_chooseQuestion.this, Activity_quiz_play.class);
+                int A = 10;
+                Bundle bundle = new Bundle();
+                bundle.putInt("SL1", A);
+                bundle.putInt("K", 0);
+                bundle.putInt("H", 0);
+                bundle.putInt("C", 0);
+                bundle.putInt("B", 0);
+                bundle.putInt("D", 0);
+                bundle.putInt("Z", 0);
+                intent.putExtra("MyPackage1", bundle);
+                startActivity(intent);
+                finish();
+            }
+        });
 //    public void openActivity_quiz_play() {
 //        Intent intent = new Intent(this, Activity_quiz_play.class);
 //        startActivity(intent);
 //    }
-}
+    }}
